ef9b02e1946373fb1787b86a3d5298de4f061f9e
vodinh <vodinh90@gmail.com>
2014-03-24 22:46:05 +0700

    Test repository with query JOIN multiple tables

--- src/main/java/com/rick/management/car/spring/persit/repositories/UserRepo.java
+++ src/main/java/com/rick/management/car/spring/persit/repositories/UserRepo.java
@@ -3,16 +3,17 @@ package com.rick.management.car.spring.persit.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.rick.management.car.spring.persit.BaseRepo;
-import com.rick.management.car.spring.persit.domain.User;
 import com.rick.management.car.spring.persit.domain.*;
-
 
 public interface UserRepo extends BaseRepo<User, Integer>, UserCustomRepo {
 	@Query("SELECT u FROM User u ")
 	public List<User> findAll();
-	
-	
-//	public List<User> findByQuyenId(Integer quyenId);
+
+	public static final String FIND_BY_QUYEN_ID = "SELECT u FROM User u JOIN u.dmQuyens dq JOIN dq.quyen q WHERE q.quyenId = :quyenId AND dq.trangThai = true";
+
+	@Query(FIND_BY_QUYEN_ID)
+	public List<User> findByQuyenId(@Param("quyenId") Integer quyenId);
 }
